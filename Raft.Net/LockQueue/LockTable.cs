7ee138fb192382e94bdc589a3d64d9a7a3ced122
hhz <314533210@qq.com>
2020-04-25 22:13:28 +0800

    lock project

--- Raft.Net/LockQueue/LockTable.cs
+++ Raft.Net/LockQueue/LockTable.cs
@@ -2,25 +2,57 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace LockQueue
 {
     public class LockTable
     {
-        public ConcurrentDictionary<string, LockQueue> Queues = new ConcurrentDictionary<string, LockQueue>();
+        HashEntry[] table = null;
+        int size = 100;
+        public LockTable(int size=100)
+        {
+            table = new HashEntry[size];
+            for (int i=0;i<size;i++)
+            {
+                table[i] = new HashEntry();
+            }
+            this.size = size;
+        }
 
         public LockQueue GetQueue(string key)
         {
-            if (!Queues.ContainsKey(key))
+            int hashCode = key.GetHashCode();
+            int index = hashCode % size;
+            HashEntry entry = table[index];
+            do
             {
-                var queue = new LockQueue();
-                bool success=Queues.TryAdd(key, queue);
-                if (success) return queue;
-                else return null;
-            }else
-            {
-                return Queues[key];
-            }
+                if (entry.key == key) return entry.queue;
+                if (entry.Next==null)
+                {
+                    HashEntry newEntry = new HashEntry()
+                    {
+                        key = key
+                    };
+                    var update=Interlocked.CompareExchange(ref entry.Next, null, newEntry);
+                    if (update == newEntry) return update.queue;
+                    else continue;
+                }else
+                {
+                    entry = entry.Next;
+                }
+                 
+            } while (entry.Next != null);
+
+            //should not happen
+            return null;
+
         }
+    }
+    public class HashEntry
+    {
+        public string key { get; set; }
+        public HashEntry Next = null;
+        public LockQueue queue = new LockQueue();
     }
 }
