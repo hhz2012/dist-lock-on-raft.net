c129eb590d879a99f78f382ba8330c17e208d11c
hhz <314533210@qq.com>
2020-04-25 22:01:38 +0800

    add lock project

---
+++ Raft.Net/LockQueue/LockQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace LockQueue
+{
+    public class LockQueue
+    {
+        LockEntry header = new LockEntry();
+        public bool LockNoWait(string sessionId, LockType type)
+        {
+            var item = header;
+            while (header.Type==type&&type!=LockType.Write)
+            {
+                if (header.Next==null)
+                {
+                    //find tail 
+                    LockEntry newEntry = new LockEntry()
+                    {
+                        sessionId = sessionId,
+                        Type = type
+                    };
+                    var update=Interlocked.CompareExchange(ref header.Next, null, newEntry);
+                    if (update == newEntry) return true;
+                    else continue; //other body take this position
+                }
+            };
+            return false;
+        }
+    }
+    public class LockEntry
+    {
+        public string sessionId { get; set; }
+        public LockType Type { get; set; }
+        public LockEntry Next  = null;
+    }
+    public enum LockType
+    {
+        Read=0,
+        Write=1
+    }
+}
---
+++ Raft.Net/LockQueue/LockTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LockQueue
+{
+    public class LockTable
+    {
+        public ConcurrentDictionary<string, LockQueue> Queues = new ConcurrentDictionary<string, LockQueue>();
+
+        public LockQueue GetQueue(string key)
+        {
+            if (!Queues.ContainsKey(key))
+            {
+                var queue = new LockQueue();
+                bool success=Queues.TryAdd(key, queue);
+                if (success) return queue;
+                else return null;
+            }else
+            {
+                return Queues[key];
+            }
+        }
+    }
+}
