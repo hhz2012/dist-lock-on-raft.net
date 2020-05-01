e7138740ba6caf8d1e226dd6eee8a0fdda7f37b9
hhz <314533210@qq.com>
2020-05-01 15:54:24 +0800

    d

--- Raft.Net/Raft.Core/LogStore/IStateLog.cs
+++ Raft.Net/Raft.Core/LogStore/IStateLog.cs
@@ -12,40 +12,29 @@ namespace Raft
     {
         public ulong StateLogId { get; set; } 
         public ulong StateLogTerm { get; set; }
-
         public ulong LastCommittedIndex { get; set; }
-
         public ulong LastCommittedIndexTerm { get; set; }
-
         public ulong LastBusinessLogicCommittedIndex { get; set; }
-
         public ulong LastAppliedIndex { get; set; }
         //leader operation 
 
         void AddLogEntry(StateLogEntrySuggestion suggestion);
         void AddLogEntryByFollower(StateLogEntrySuggestion suggestion);
         bool CommitLogEntry(NodeRaftAddress address, uint majorityQuantity, StateLogEntryApplied applied);
+        void RollbackToLastestCommit();
         SyncResult SyncCommitByHeartBeat(LeaderHeartbeat lhb);
 
-        void RollbackToLastestCommit();
-        //common 
+
+        //get function 
         StateLogEntry GetCommitedEntryByIndex(ulong logEntryId);
         StateLogEntry GetEntryByIndexTerm(ulong logEntryId, ulong logEntryTerm);
-
         //load local log for follower
         StateLogEntrySuggestion GetNextStateLogEntrySuggestion(StateLogEntryRequest req);
         //follower operatoin
-    
-     
         void AddFakePreviousRecordForInMemoryLatestEntity(ulong prevIndex, ulong prevTerm);
-        
-
         //void Clear_dStateLogEntryAcceptance_PeerDisconnected(string endpointsid);
         void Debug_PrintOutInMemory();
         void Dispose();
-       
-        
-       
     }
     public enum eEntryAcceptanceResult
     {
